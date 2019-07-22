dd758cb66f2a9bdc8ac24215bf72464862a33eab
Administrator <46816940@qq.com>
2019-07-22 17:21:29 +0800

    Initial commit

---
+++ src/main/java/com/teng/springloader/SpringloaderApplication.java
@@ -0,0 +1,13 @@
+package com.teng.springloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringloaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringloaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teng/springloader/SpringloaderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.teng.springloader;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringloaderApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
